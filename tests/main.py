fa989e15f3bc3f234b04ff3e7b8ebc2d00b6cf02
Jose Rostagno <joserostagno@vijona.com.ar>
2012-04-12 18:57:56 -0300

    Test: fix import

--- tests/main.py
+++ tests/main.py
@@ -1,12 +1,14 @@
 import unittest
-from src.pyfb import Pyfb
+import sys
+import os
+sys.path.append(os.path.abspath('..'))
+from pyfb import Pyfb
 
 class pyfbTests(unittest.TestCase):
     
-    def test_basic(self):        
+    def test_basic(self):
         self.assertEquals()
     
 if __name__ == "__main__":
 
     unittest.main()
-    
