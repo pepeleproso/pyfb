103c094b0f8df94fbf326b547438a98b839e1e02
Jose Rostagno <joserostagno@vijona.com.ar>
2012-04-13 19:50:50 -0300

    Test: Add a new test for exceptions

--- tests/main.py
+++ tests/main.py
@@ -2,13 +2,23 @@ import unittest
 import sys
 import os
 sys.path.append(os.path.abspath('..'))
-from pyfb import Pyfb
+from pyfb import Pyfb, OAuthException, PyfbException
+
+#Your APP ID. It Needs to register your application on facebook
+#http://developers.facebook.com/
+FACEBOOK_APP_ID = '178358228892649'
 
 class pyfbTests(unittest.TestCase):
-    
-    def test_basic(self):
-        self.assertEquals()
-    
+
+    def test_exceptions(self):
+        #instance client
+        facebook = Pyfb(FACEBOOK_APP_ID)
+
+        #Test new auth exception
+        self.assertRaises(OAuthException, facebook.get_myself)
+        #Test old and generic exception
+        self.assertRaises(PyfbException, facebook.get_myself)
+
 if __name__ == "__main__":
 
     unittest.main()
